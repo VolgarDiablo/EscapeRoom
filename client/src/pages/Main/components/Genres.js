364919ed659fe78e6f1d4095bf2993e8581b1122
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2025-02-27 13:41:48 +0200

    create component for genres

---
+++ client/src/pages/Main/components/Genres.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchGenres = async () => {
+  const { data } = await axios.get("http://localhost:8080/genre");
+  return data;
+};
+
+function Genres() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["genres"],
+    queryFn: fetchGenres,
+  });
+
+  if (isLoading) return <div>Загрузка...</div>;
+  if (error) return <div>Ошибка загрузки данных</div>;
+
+  return (
+    <div>
+      {data.map((genre) => (
+        <div key={genre.id}>
+          <img alt={genre.name} src={genre.path} />
+          <span>{genre.name} </span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Genres;
