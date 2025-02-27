f47c55f4f45337452e864c1a3022b6cf32c89097
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2025-02-27 17:40:13 +0200

    some shange

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -4,7 +4,7 @@ import Genres from "./components/Genres";
 function Main() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#141414] to-[#1F1D1D] ">
-      <div className="max-w-[1080px] mx-auto pt-[120px] flex flex-col items-start px-[15px]">
+      <div className="max-w-[1080px] mx-auto pt-[120px] flex flex-col items-start">
         <div className="text-activeText text-[14px] leading-[20.16px] font-medium">
           Квесты в Днепре
         </div>
--- client/src/pages/Main/components/Genres.js
+++ client/src/pages/Main/components/Genres.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -13,17 +13,39 @@ function Genres() {
     queryFn: fetchGenres,
   });
 
+  const [activeGenre, setActiveGenre] = useState(null);
+
   if (isLoading) return <div>Загрузка...</div>;
   if (error) return <div>Ошибка загрузки данных</div>;
 
   return (
-    <div>
-      {data.map((genre) => (
-        <div key={genre.id}>
-          <img alt={genre.name} src={genre.base64} />
-          <span>{genre.name} </span>
-        </div>
-      ))}
+    <div className="flex flex-wrap items-center mb-[20px] h-10">
+      {data.map((genre, index) => {
+        return (
+          <div
+            key={genre.id}
+            className={`flex flex-row items-center cursor-pointer gap-3 mr-10 text-[#E6E6E6] ${
+              index === 0 ? "ml-0" : "ml-10"
+            }`}
+          >
+            <img
+              alt={genre.name}
+              src={genre.base64}
+              className="w-[30px] h-[30px]"
+            />
+            <span
+              className={`text-[14px] leading-[19.6px] ${
+                activeGenre === genre.id
+                  ? "border-b-2 border-solid border-[#F2890F] text-[#E6E6E6]"
+                  : ""
+              } `}
+              onClick={() => setActiveGenre(genre.id)}
+            >
+              {genre.name}
+            </span>
+          </div>
+        );
+      })}
     </div>
   );
 }
