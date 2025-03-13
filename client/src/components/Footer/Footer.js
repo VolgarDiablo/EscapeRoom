021d3e6ece2af2b5a7e7247b932386c85302de3a
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2025-03-13 17:31:00 +0200

    add svg

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="max-w-[1300px]">
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
