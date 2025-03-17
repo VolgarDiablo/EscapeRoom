da4d11510663b8795f80d9d2ce9460a3957ed873
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2025-03-17 17:46:54 +0200

    update text

--- client/src/pages/Quest/RenderQuest.js
+++ client/src/pages/Quest/RenderQuest.js
@@ -2,6 +2,10 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import People from "../../assets/icons/People";
+import Puzzle from "../../assets/icons/Puzzle";
+import Clock from "../../assets/icons/Clock";
+import SeparatorBig from "../../assets/icons/SeparatorBig";
 
 const fetchQuest = async (id) => {
   const { data } = await axios.get(`http://localhost:8080/quest/${id}`);
@@ -24,13 +28,44 @@ function RenderQuest() {
 
   return (
     <div>
-      Выбранный квест ID: {id}
       <div
-        className="fixed inset-0 w-full h-full bg-cover bg-no-repeat bg-center"
+        className="fixed inset-0 w-full h-full bg-cover bg-no-repeat bg-center z-0"
         style={{
           backgroundImage: `url(${data.base64})`,
         }}
       ></div>
+      <div className="absolute top-1/2 right-16 transform -translate-y-1/2 max-w-[680px] p-6 text-white text-left">
+        <span className="text-sm font-medium text-[#F2890F]">
+          {data.category}
+        </span>
+
+        <h1 className="uppercase text-[92px] font-black">{data.title}</h1>
+
+        <div className="flex gap-6 text-[#E5E5E5] text-[14px] font-normal pb-5">
+          <span className="flex items-center gap-2 text-[#FFFFFF] leading-[95%]">
+            <Clock />
+            {data.duration}
+          </span>
+          <SeparatorBig />
+          <span className="flex items-center gap-2">
+            <People />
+            {data.players} чел
+          </span>
+          <SeparatorBig />
+          <span className="flex items-center gap-2">
+            <Puzzle />
+            {data.difficulty}
+          </span>
+        </div>
+
+        <p className="text-[#E5E5E5] text-[15px] leading-[150%] font-medium">
+          {data.description}
+        </p>
+
+        <button className="mt-6 bg-orange-500 hover:bg-orange-600 text-white py-2 px-6 rounded-lg font-bold">
+          ЗАБРОНИРОВАТЬ
+        </button>
+      </div>
     </div>
   );
 }
